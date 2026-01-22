280a054b159f37c38d60b4c4672278584854615f
Onagaki <charlesmarcnoel@protonmail.com>
2026-01-22 01:45:54 +0100

    Python script created

---
+++ mystical8ball.py
@@ -0,0 +1,28 @@
+import random
+
+answer = " "
+rand = random.randint(0, 8)
+
+Question = str(input("What is your question :"))
+
+if rand == 0:
+    answer = "Yes - definitely."
+elif rand == 1:
+    answer = "It is decidely so."
+elif rand == 2:
+    answer = "Without a doubt."
+elif rand == 3:
+    answer = "Reply hazy, try again."
+elif rand == 4:
+    answer = "Ask again later."
+elif rand == 5:
+    answer = "Better not tell you now."
+elif rand == 6:
+    answer = "My sources say no."
+elif rand == 7:
+    answer = "Outlook not so good."
+elif rand == 8:
+    answer = "Very doubtful."
+
+print(Question)
+print(answer)
